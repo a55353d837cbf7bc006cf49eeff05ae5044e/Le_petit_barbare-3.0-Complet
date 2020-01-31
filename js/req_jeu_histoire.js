9731fed0880c3fa8a1cfcc040e8eaad6130351c7
TheBarnacle <56112873+TheBarnacle@users.noreply.github.com>
2020-01-31 01:12:16 +0100

    fix syntax error - patch colissions new maps

--- js/req_jeu_histoire.js
+++ js/req_jeu_histoire.js
@@ -12,7 +12,7 @@ const req_jeu_histoire = function (req, res, query, bfld, heros, oppo, wave, niv
 	
 	let marqueurs;
 	let page;
-	let tmp = {"x" : 3, "y" : 2, "life" : 100, "scry" : 1, "epee" : 0, "hache" : 0, "dague" : 0, "masse" : 0, "potion" : 0, "epee_1": 0, "epee_2": 1, "arc" : 0, "pieces" : 0};
+	let tmp = {"x" : 3, "y" : 2, "life" : 100, "scry" : 1, "epee" : 0, "hache" : 0, "dague" : 0, "masse" : 0, "potion" : 0, "epee_1": 0, "epee_2": 0, "arc" : 0, "pieces" : 0};
 	if(niveau[0] === 1){
 		heros.splice(0, 1);
 		heros.push(tmp);
@@ -21,6 +21,166 @@ const req_jeu_histoire = function (req, res, query, bfld, heros, oppo, wave, niv
 		heros[0].y = 2;
 	}	
 
+	if(niveau[0] === 3 ||niveau[0] === 9 || niveau[0] === 13 || niveau[0] === 19){
+		for(let i = 0; i < bfld[1].length; i++){
+			bfld[1][i] = "1";
+		}
+	}else if(niveau[0] === 4 || niveau[0] === 5){
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[1][i] = "1";
+        }
+		for(let j = 0; j < bfld.length; j++){
+			bfld[j][13] = "1";
+		}
+	}else if(niveau[0] === 32){
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[0][i] = "1";
+            bfld[6][i] = "1";
+            bfld[1][i] = " ";
+            bfld[2][i] = " ";
+            bfld[3][i] = " ";
+            bfld[4][i] = " ";
+            bfld[5][i] = " ";
+        }
+		bfld[1][0] = "1";
+        bfld[1][1] = "1";
+        bfld[1][2] = "1";
+        bfld[1][14] = "1";
+
+        bfld[2][0] = "1";
+        bfld[2][1] = "1";
+        bfld[2][2] = "1";
+        bfld[2][14] = "1";
+
+        bfld[3][0] = "1";
+        bfld[3][1] = "1";
+        bfld[3][2] = "1";
+        bfld[3][14] = "1";
+
+        bfld[4][0] = "1";
+        bfld[4][1] = "1";
+        bfld[4][2] = "1";
+        bfld[4][14] = "1";
+
+        bfld[5][0] = "1";
+        bfld[5][1] = "1";
+        bfld[5][2] = "1";
+		
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[1][i] = "1";
+        }
+
+	}else if(niveau[0] === 6){
+		 for(let i = 0; i < bfld[1].length; i++){
+            bfld[5][i] = "1";
+        	bfld[4][i] = "1";
+		}
+	}else if(niveau[0] === 31){
+		for(let j = 0; j < bfld.length; j++){
+            bfld[j][13] = "1";
+			bfld[j][12] = "1";
+			bfld[j][11] = "1";
+        }
+	}else if(niveau[0] === 16){
+		for(let j = 0; j < bfld.length; j++){
+            bfld[j][13] = "1";
+            bfld[j][12] = "1";
+        }
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[5][i] = "1";
+        }
+		bfld[0][2] = "1";
+		bfld[1][2] = "1";
+		bfld[4][2] = "1";
+		bfld[5][2] = "1";
+		bfld[6][2] = "1";
+	}else if(niveau[0] === 28){
+		for(let j = 0; j < bfld.length; j++){
+            bfld[j][2] = "1";
+            bfld[j][3] = "1";
+        }
+	}else if(niveau[0] === 29){
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[0][i] = "1";
+            bfld[6][i] = "1";
+            bfld[1][i] = " ";
+            bfld[2][i] = " ";
+            bfld[3][i] = " ";
+            bfld[4][i] = " ";
+            bfld[5][i] = " ";
+        }
+
+		bfld[1][0] = "1";
+        bfld[1][1] = "1";
+        bfld[1][2] = "1";
+        bfld[1][14] = "1";
+
+        bfld[2][0] = "1";
+        bfld[2][1] = "1";
+        bfld[2][2] = "1";
+        bfld[2][14] = "1";
+
+        bfld[3][0] = "1";
+        bfld[3][1] = "1";
+        bfld[3][2] = "1";
+        bfld[3][14] = "1";
+
+        bfld[4][0] = "1";
+        bfld[4][1] = "1";
+        bfld[4][2] = "1";
+        bfld[4][14] = "1";
+
+        bfld[5][0] = "1";
+        bfld[5][1] = "1";
+        bfld[5][2] = "1";
+        bfld[5][14] = "1";
+
+		
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[1][i] = "1";
+        }
+		bfld[5][13] = "1";
+        bfld[5][12] = "1";
+        bfld[5][11] = "1";
+        bfld[5][10] = "1";
+        bfld[5][9] = "1";
+		bfld[5][8] = "1";
+	}else{
+		for(let i = 0; i < bfld[1].length; i++){
+            bfld[0][i] = "1";
+			bfld[6][i] = "1";
+			bfld[1][i] = " ";
+			bfld[2][i] = " ";
+			bfld[3][i] = " ";
+			bfld[4][i] = " ";
+			bfld[5][i] = " ";
+        }
+		bfld[1][0] = "1";
+		bfld[1][1] = "1";
+		bfld[1][2] = "1";
+		bfld[1][14] = "1";
+
+		bfld[2][0] = "1";
+        bfld[2][1] = "1";
+        bfld[2][2] = "1";
+        bfld[2][14] = "1";
+
+		bfld[3][0] = "1";
+        bfld[3][1] = "1";
+        bfld[3][2] = "1";
+        bfld[3][14] = "1";
+
+		bfld[4][0] = "1";
+        bfld[4][1] = "1";
+        bfld[4][2] = "1";
+        bfld[4][14] = "1";
+		
+		bfld[5][0] = "1";
+        bfld[5][1] = "1";
+        bfld[5][2] = "1";
+        bfld[5][14] = "1";
+	}
+	
 	
 	for(let i = 1; i < bfld.length - 1; i ++){
 		for(let j = 1; j< bfld[0].length - 1; j++){
@@ -32,7 +192,14 @@ const req_jeu_histoire = function (req, res, query, bfld, heros, oppo, wave, niv
 		}
 	}
 
-	bfld[3][2] = "x";
+	if(niveau[0] === 28){
+		bfld[1][3] = "x";
+		heros[0].x = 1;
+        heros[0].y = 3;
+	}else{
+		bfld[3][2] = "x";
+	}
+	
 	wave[0] = 0;
 	for(let k = 0; k < oppo.length; k++){
 		oppo.splice(k, 1);
